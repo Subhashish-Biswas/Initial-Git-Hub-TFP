8aeb0a6a22e4a92a90293fb05d29269a6790ded7
Subhashish Biswas <subhashish.0210@gmail.com>
2021-05-25 21:08:44 +0530

    initial commit

---
+++ Assignment3_1.cpp
@@ -0,0 +1,22 @@
+//Program to enter a positive integer and find whether a number is prime.
+//Subhashish Biswas 08 CSE
+#include<stdio.h>
+int main(){
+    printf("Enter a positive number\n");
+    int a;
+    int count=0;
+
+    scanf("%d",&a);
+        for(int j=1;j<=a;j++){
+            if(a%j==0)
+            count++;}
+            if(count==2)
+                printf("The number is prime.\n");
+            else
+                printf("The number is not prime.\n");       
+
+   return 0;
+    }
+       
+
+
---
+++ demo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Hii How are you man."<< endl;
+    return 0;
+}
